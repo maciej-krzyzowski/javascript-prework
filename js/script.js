b77471991adbf64c730bf25274d928f782b4e647
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2019-10-24 16:47:49 +0200

    Add text

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ function printMessage(msg) {
 function clearMessages() {
     document.getElementById('messages').innerHTML = '';
 }
+
+var computerMove, playerMove;
+playerMove = 'Papier. ';
+computerMove = 'kamień';
+printMessage(playerMove + 'Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
